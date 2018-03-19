4e8b304ae40c751578d01d1f4d4ebb0ab7871cc2
kirin1218 <kirin1218@outlook.jp>
2018-03-19 13:11:15 +0900

    edit sample525

--- sample521.py
+++ sample521.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -5,6 +7,45 @@ train = np.loadtxt('click.csv', delimiter=',', skiprows=1)
 train_x = train[:,0]
 train_y = train[:,1]
 
-plt.plot(train_x, train_y, 'o')
-plt.show()
+#plt.plot(train_x, train_y, 'o')
+#plt.show()
+
+theta0 = np.random.rand()
+theta1 = np.random.rand()
+
+def f(x):
+    return theta0 + theta1*x
+
+def E(x,y):
+    return 0.5 * np.sum(( y - f(x))**2)
+
+mu = train_x.mean()
+sigma = train.std()
+
+def standardize(x):
+    return (x - mu)/sigma
+
+train_z = standardize(train_x)
+
+#plt.plot(train_z, train_y, 'o')
+#plt.show()
+
+ETA = 1e-3
+
+diff = 1
+
+count = 0
+
+error = E(train_z, train_y)
+while diff > 1e-2:
+    tmp0 = theta0 - ETA* np.sum((f(train_z) - train_y))
+    tmp1 = theta1 - ETA* np.sum((f(train_z) - train_y) * train_z)
+    theta0 = tmp0
+    theta1 = tmp1
+    current_error = E(train_z, train_y)
+    diff = error - current_error
+    error = current_error
+    count += 1
+    log = '{}: theta0 = {:.3f}, theta1= {:.3f}, diff= {:.4f}'
+    print(log.format(count, theta0, theta1,diff))
 
