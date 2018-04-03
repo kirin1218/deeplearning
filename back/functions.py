2b9be5c99bdfd98d5e5998d99f8c83f6c47ef122
kirin1218 <kirin1218@outlook.jp>
2018-04-04 08:04:02 +0900

    make mnist func

---
+++ back/functions.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+#   ソフトマックス関数
+def softmax(a):
+    exp_a = np.exp(a)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
+
+#   シグモイド関数
+def sigmoid(x):
+    return 1/1+np.exp(-x)
+
+#   交差エントロピー誤差
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    #   教師データがone-hot-vectorの場合、正解ラベルのインデックスに変換
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
